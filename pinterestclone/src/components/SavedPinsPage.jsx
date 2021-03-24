056959062876b0d42e0773fc719873c81bb2ee30
massiliatazit <massiliatazit@users.noreply.github.com>
2021-03-25 00:41:33 +0100

    styling

--- pinterestclone/src/components/SavedPinsPage.jsx
+++ pinterestclone/src/components/SavedPinsPage.jsx
@@ -3,6 +3,7 @@ import unsplash from "../api/unsplash";
 import { connect } from "react-redux";
 import Card from "./Card";
 import "../styles/index.css";
+import styled from "styled-components";
 import { Row, Col, Container } from "react-bootstrap";
 const mapStateToProps = (state) => state;
 function SavedPinsPage(props) {
@@ -16,13 +17,66 @@ function SavedPinsPage(props) {
   }, []);
   return (
     <>
-      <Container className="hero px-4 mb-5" fluid></Container>
-      {pins.length > 0 && (
+      <Container className=" px-4 mb-5 d-flex  justify-content-center align-items-center">
+        <CenteredContainer>
+          {pins.length > 0 && (
+            <div
+              style={{
+                overflow: "hidden",
+                position: "absolute",
+                top: "15%",
+                left: "24%",
+                borderRadius: 30,
+                maxHeight: 369,
+              }}
+            >
+              <div
+                style={{
+                  //position: "absolute",
+                  transform: "rotate(-25deg) scale(1.5)",
+                  marginLeft: "1rem",
+                }}
+              >
+                {[0, 1, 2, 3].map((i) => (
+                  <Row>
+                    {pins
+                      .slice(0, 4)
+                      .sort(() => 0.5 - Math.random())
+                      .map((pin) => (
+                        <Col className="pr-1 mb-1">
+                          <div
+                            style={{
+                              overflow: "hidden",
+                              maxWidth: "170px",
+                              maxHeight: "130px",
+                            }}
+                          >
+                            <img
+                              src={pin.urls.small}
+                              className=" img-fluid "
+                              style={{
+                                objectFit: "cover",
+                                maxWidth: "100%",
+                                maxHeight: "100%",
+                              }}
+                              alt="signupPins"
+                            />
+                          </div>
+                        </Col>
+                      ))}
+                  </Row>
+                ))}
+              </div>
+            </div>
+          )}
+        </CenteredContainer>
+      </Container>
+      {/* {pins.length > 0 && (
         <div style={styles.pin_container}>
           <Card size="medium" />
           <Card size="medium" />
         </div>
-      )}
+      )} */}
     </>
   );
 }
@@ -44,3 +98,10 @@ const styles = {
 };
 
 export default connect(mapStateToProps)(SavedPinsPage);
+const CenteredContainer = styled.div`
+  max-width: 656px;
+  box-sizing: border-box;
+  backgound-color: blue;
+  height: 100%;
+  width: 100%;
+`;

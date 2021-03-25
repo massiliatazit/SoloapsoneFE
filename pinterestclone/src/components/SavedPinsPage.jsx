43e821923e3a33069551ceed129edd016934c615
massiliatazit <massiliatazit@users.noreply.github.com>
2021-03-25 01:22:09 +0100

    more items

--- pinterestclone/src/components/SavedPinsPage.jsx
+++ pinterestclone/src/components/SavedPinsPage.jsx
@@ -18,7 +18,7 @@ function SavedPinsPage(props) {
   }, []);
   return (
     <>
-      <Container className=" px-4 mb-5 d-flex  justify-content-center align-items-center">
+      <Container className=" px-4 mb-5 d-flex  flex-column justify-content-center align-items-center">
         <CenteredContainer>
           {pins.length > 0 && (
             <div
@@ -72,6 +72,65 @@ function SavedPinsPage(props) {
             </div>
           )}
         </CenteredContainer>
+
+        <Row>
+          <Col>
+            <div
+              style={{ width: "100%", textAlign: "center", marginTop: "205%" }}
+            >
+              <img
+                src="https://i.imgur.com/D5wcicT.jpg"
+                alt="photo"
+                width="120px"
+                height="120px"
+                style={{ borderRadius: "60px" }}
+              />
+              <h3 style={{ fontWeight: "bold", marginTop: "10px" }}>
+                {props.user.name}
+              </h3>
+              <div>{props.user.email}</div>
+              <div>
+                {props.user.following.length} following •{" "}
+                {props.user.followers.length} followers
+              </div>
+              <h1 style={{ marginTop: "40px", color: "darkred" }}>
+                Saved Pins
+              </h1>
+              {/* <div style={{width:"100%", marginTop:"100px", display:"flex", justifyContent:"space-evenly"}}> 
+                    <div>
+                        <div style={{width:"230px", height:"150px", border:"1px solid black", borderRadius:"20px"}}>LOVE</div>
+                        <br/>
+                        <div>All Pins</div>
+                    </div>
+                    {
+                        curruser.saved_pins.map( pin => (
+                            <div  key={pin.id} >
+                                <div style={{width:"230px", height:"150px", border:"1px solid black", borderRadius:"20px"}}>LOVE</div><br/>
+                                <div>{pin.title}</div>
+                            </div>
+                        ))
+                    }
+                </div> */}
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  flexWrap: "wrap",
+                  flexDirection: "row",
+                }}
+              >
+                {props.user.saved?.map((pin) => (
+                  <ImageCard>
+                    <Image src={pin.urls.regular} alt="pin" />
+                    <h3 style={{ margin: "10px" }}>
+                      Photo by {pins.user.username}
+                    </h3>
+                  </ImageCard>
+                ))}
+              </div>
+            </div>
+          </Col>
+        </Row>
       </Container>
       {/* {pins.length > 0 && (
         <div style={styles.pin_container}>
@@ -106,4 +165,23 @@ const CenteredContainer = styled.div`
   backgound-color: blue;
   height: 100%;
   width: 100%;
+`;
+const Wrapper = styled.div`
+  box-sizing: border-box;
+  max-width: 79%;
+  height: 700px;
+  display: flex;
+  flex-flow: column wrap;
+`;
+
+const ImageCard = styled.div`
+  width: 18%;
+  margin: 10px;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 300px;
+  background-size: cover;
+  border-radius: 20px;
 `;

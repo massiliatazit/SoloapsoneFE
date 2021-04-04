ae82bcd92102dfb4935349dff2edbed7e2d931e9
massiliatazit <massiliatazit@users.noreply.github.com>
2021-04-04 17:15:08 +0200

    notifications added

--- pinterestclone/src/components/LoginPage/LoginModal.jsx
+++ pinterestclone/src/components/LoginPage/LoginModal.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import unsplash from "../../api/unsplash";
 import { Container, Row, Col } from "react-bootstrap";
 
 function LoginModal(props) {
+  const [pins, setPins] = useState([]);
+  const getPins = async () => {
+    unsplash.get(`https://api.unsplash.com/topics`, {}).then((res) => {
+      let results = res.data;
+      console.log(results);
+      setPins(results);
+    });
+  };
+  useEffect(() => {
+    getPins();
+  }, []);
+
   return (
     <Container fluid>
       <Row>
         {Array.from(
           new Set(
-            props.pins.slice(0, 6).map((i) => (
-              <Col xs={3} lg={2} className="pr-1">
-                {props.pins
+            pins.slice(0, 6).map((i) => (
+              <Col xs={3} lg={6} className="pr-1">
+                {pins
                   .sort(() => 0.5 - Math.random())
                   .slice(0, 6)
 
-                  .map((pin) => (
-                    <img
-                      src={pin.urls.regular}
-                      className="item img-fluid"
-                      style={{ objectFit: "cover" }}
-                      alt="signupPins"
-                    />
-                  ))}
+                  .map((pin) =>
+                    pin.preview_photos.map((image) => {
+                      <img
+                        src={image.urls.regular}
+                        className="item img-fluid"
+                        style={{ objectFit: "cover" }}
+                        alt="signupPins"
+                      />;
+                    })
+                  )}
               </Col>
             ))
           )
---
+++ pinterestclone/src/components/Notifications.jsx
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import "antd/dist/antd.css";
+import { Drawer } from "antd";
+import { Menu, Dropdown } from "antd";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+const Icon = styled.img`
+  padding: 12px;
+  height: 50px;
+  opacity: 0.6;
+  margin-left: 5px;
+  border-radius: 50%;
+  &:hover {
+    background-color: #eee;
+    cursor: pointer;
+  }
+`;
+const Posts = styled.div`
+  width: 100%;
+  border-radius: 20px;
+  overflow: hidden;
+  font-size: 16px;
+  margin-bottom: 20px;
+  display: flex;
+  & img:hover {
+    cursor: pointer;
+  }
+`;
+
+const Image = styled.img`
+  height: 150px;
+  width: 100px;
+  border: 1px solid white;
+`;
+
+const Notifications = () => {
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+  const menu = (
+    <Menu>
+      <Menu.Item key="0">
+        <a href="https://www.antgroup.com">
+          {" "}
+          <p>You have got new ideas waiting for you</p>
+          <Posts>
+            <div>
+              <Image
+                src="https://i.pinimg.com/290x/d3/bb/39/d3bb392c0d89fa4dd24cf51c0e75e160.jpg"
+                alt=""
+                style={{ height: "150px", width: "100px" }}
+              />
+            </div>
+            <div>
+              <Image
+                src="https://i.pinimg.com/290x/7d/f1/a9/7df1a95f160e44ec73aed5a62f463c94.jpg"
+                alt=""
+                style={{ height: "150px", width: "100px" }}
+              />
+            </div>
+            <div>
+              <Image
+                src="https://i.pinimg.com/290x/80/7e/95/807e95f24d1d6beb566439027db0ffe5.jpg"
+                alt=""
+                style={{ height: "150px", width: "100px" }}
+              />
+            </div>
+          </Posts>
+        </a>
+      </Menu.Item>
+      <Menu.Item key="1">
+        <a href="https://www.aliyun.com">
+          <p>Some new Ideas are Wating for you</p>
+          <Posts>
+            <div>
+              <Image
+                src="https://i.pinimg.com/236x/5a/32/90/5a32905c3a7676f674c62c57b87aaf85.jpg"
+                alt=""
+                style={{ height: "150px", width: "100px" }}
+              />
+            </div>
+            <div>
+              <Image
+                src="https://i.pinimg.com/236x/3e/e4/b2/3ee4b2eeb0e594fc571a02d54615aaf4.jpg"
+                alt=""
+                style={{ height: "150px", width: "100px" }}
+              />
+            </div>
+            <div>
+              <Image
+                src="https://i.pinimg.com/236x/a7/6b/f0/a76bf0e5a791c2027ee9346b86f1e303.jpg"
+                alt=""
+                style={{ height: "150px", width: "100px" }}
+              />
+            </div>
+          </Posts>
+        </a>
+      </Menu.Item>
+      <Menu.Divider />
+    </Menu>
+  );
+
+  return (
+    <>
+      <Dropdown overlay={menu} trigger={["click"]} placement="bottomCenter">
+        <div>
+          <NotificationsIcon
+            onClick={(e) => e.preventDefault()}
+          ></NotificationsIcon>{" "}
+        </div>
+      </Dropdown>
+    </>
+  );
+};
+export default Notifications;

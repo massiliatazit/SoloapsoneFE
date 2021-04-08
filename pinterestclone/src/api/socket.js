ca165aef15ac2ac073b13fa8a53889a6778b6bc6
massiliatazit <massiliatazit@users.noreply.github.com>
2021-04-08 16:19:12 +0200

    socket emit added

--- pinterestclone/src/api/socket.js
+++ pinterestclone/src/api/socket.js
@@ -1,10 +1,25 @@
-// import io from "socket.io-client";
+import io from "socket.io-client";
 
-// let socket = io(process.env.REACT_APP_URL)
-// export const joinRoom = (data)=>{
-//     socket.emit("join",data)
-// }
+let socket = io(process.env.REACT_APP_URL)
+export const joinRoom = (data)=>{
+    socket.emit("join",data)
+}
 
+  
+  export const leaveRoom = (data) => {
+    socket.emit("LEAVE_ROOM", data);
+  };
+  
+ 
+  
+  export const listenChat = ( data) => {
+    
+    socket.on("CHAT_MESSAGE", data);
+  };
+  
+  export const sendChat = (data) => {
+    socket.emit("CHAT_MESSAGE", data);
+  };
 
 
 
